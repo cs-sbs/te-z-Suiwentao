6d11c6ce7a2171f3ea2d9282cee21370e487f30c
Suiwentao <1632156722@qq.com>
2024-10-23 23:50:56 +0800

    Add files via upload

--- java/org/example/OddEvenCount.java
+++ java/org/example/OddEvenCount.java
@@ -1,5 +1,3 @@
-package org.example;
-
 import java.util.Scanner;
 
 public class OddEvenCount {
@@ -9,17 +7,23 @@ public class OddEvenCount {
         int evenCount = 0;
 
         while (true) {
-            int num = scanner.nextInt(); // 读取整数输入
-            if (num == -1) { // 输入 -1 时结束输入
-                break;
-            }
-            if (num % 2 == 0) { // 偶数判断
-                evenCount++;
-            } else { // 奇数判断
-                oddCount++;
+            System.out.println("请输入一个整数（输入-1结束）：");
+            if (scanner.hasNextInt()) {
+                int num = scanner.nextInt(); // 读取整数输入
+                if (num == -1) { // 输入 -1 时结束输入
+                    break;
+                }
+                if (num % 2 == 0) { // 偶数判断
+                    evenCount++;
+                } else { // 奇数判断
+                    oddCount++;
+                }
+            } else {
+                System.out.println("无效输入，请输入一个整数！");
+                scanner.next(); // 清除错误输入
             }
         }
 
-        System.out.println(oddCount + " " + evenCount); // 输出奇数个数和偶数个数
+        System.out.println("奇数个数：" + oddCount + "，偶数个数：" + evenCount); // 输出奇数个数和偶数个数
     }
 }
